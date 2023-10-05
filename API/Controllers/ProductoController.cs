2acdc082d366a9f3a16ba2a1bdde5aa3cec71abe
Alejandro Muñoz <mdiegoalejandro17@gmail.com>
2023-10-05 16:21:02 -0500

    feat: :construction: ProductoController

--- API/Controllers/ProductoController.cs
+++ API/Controllers/ProductoController.cs
@@ -1,10 +1,13 @@
-
 using Domain.Interface;
 using API.Helpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Domain.Entities;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
+using System.Threading.Tasks;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
@@ -13,70 +16,80 @@ using iText.Kernel.Geom;
 using iText.Kernel.Colors;
 using AutoMapper;
 using API.Dtos;
-namespace API.Controllers;
+using iText.Layout.Borders;
 
-public class ProductoController : BaseApiController
+namespace API.Controllers
 {
-    private IUnitOfWork _unitOfWork;
-    private readonly IMapper _mapper;
-    public ProductoController(IUnitOfWork unitOfWork, IMapper mapper){
-        _unitOfWork = unitOfWork;
-        _mapper = mapper;
-    }
-    [HttpGet("pdf")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<Pager<ProductoDto>>> Get11([FromQuery] Params productParams)
+    public class ProductoController : BaseApiController
     {
-        var products = await _unitOfWork.Productos.paginacion(productParams.PageIndex,productParams.PageSize,productParams.Search);
-        var lstProductDto = _mapper.Map<List<ProductoDto>>(products.registros);
-        MemoryStream ms = new MemoryStream();
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        // Crear un nuevo documento PDF
-        PdfWriter pdfWriter = new PdfWriter(ms);
-        PdfDocument pdfDoc = new PdfDocument(pdfWriter);
-        Document doc = new Document(pdfDoc, PageSize.LETTER);
-        doc.SetMargins(75,35,70,35);
-
-        //Agregar texto
-        doc.Add(new Paragraph("Hello Niggers i like pennne")
-        .SetFontColor(ColorConstants.BLACK));
-        PropertyInfo[] propiedades = typeof(ProductoDto).GetProperties();
-        //Agregar Tabla
-        Table table = new Table(propiedades.Length);
-        table.SetHorizontalAlignment(HorizontalAlignment.CENTER);
-        
-
-        // Imprimir encabezados de la tabla
-        foreach (var propiedad in propiedades)
+        public ProductoController(IUnitOfWork unitOfWork, IMapper mapper)
         {
-            Cell cell = new Cell().Add(new Paragraph(propiedad.Name));
-            table.AddCell(cell);
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
-        // Imprimir datos de la lista
-        foreach (var objeto in lstProductDto)
+        [HttpGet("pdf")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Pager<ProductoDto>>> Get11([FromQuery] Params productParams)
         {
+            var products = await _unitOfWork.Productos.paginacion(productParams.PageIndex, productParams.PageSize, productParams.Search);
+            var lstProductDto = _mapper.Map<List<ProductoDto>>(products.registros);
+            MemoryStream ms = new MemoryStream();
+
+            // Crear un nuevo documento PDF
+            PdfWriter pdfWriter = new PdfWriter(ms);
+            PdfDocument pdfDoc = new PdfDocument(pdfWriter);
+            Document doc = new Document(pdfDoc, PageSize.LETTER);
+            doc.SetMargins(75, 35, 70, 35);
+            doc.SetTextAlignment(TextAlignment.CENTER);
+
+            // Agregar Tabla
+            Table table = new Table(products.totalRegistros);
+            table.SetMarginTop(10); // Ajusta el espacio entre la tabla y el contenido superior del documento
+            table.SetHorizontalAlignment(HorizontalAlignment.CENTER);
+
+            // Agregar encabezados a la tabla
+            PropertyInfo[] propiedades = lstProductDto[0].GetType().GetProperties();
             foreach (var propiedad in propiedades)
             {
-                var valor = propiedad.GetValue(objeto);
-                Cell cell1 = new Cell().Add(new Paragraph(valor.ToString()));
-                table.AddCell(cell1);
-            }
-        }
-        // Agregar elemento
+                Cell headerCell = new Cell().Add(new Paragraph(propiedad.Name)
+                    .SetBold() //texto en negrita
+                    .SetFontSize(10) //tamaño de letra
+                    .SetBackgroundColor(new DeviceRgb(255, 215, 0)) //color de fondo
+                    .SetPadding(10)); // ajustar el relleno
+                headerCell.SetTextAlignment(TextAlignment.CENTER);
 
-        List listanueva = new List()
-                .Add(new ListItem("elemnto 1"))
-                .Add(new ListItem("Elemento 2"));
-        
-        doc.Add(listanueva);
-        doc.Add(table);
-        doc.Close();
-        byte[] bytesStream  = ms.ToArray();
-        ms = new MemoryStream();
-        ms.Write(bytesStream, 0, bytesStream.Length);
-        ms.Position = 0;
-        return new FileStreamResult(ms, "application/pdf");
+                table.AddCell(headerCell);
+            }
+
+            // Agregar datos a la tabla
+            foreach (var x in lstProductDto)
+            {
+                foreach (var propiedad in propiedades)
+                {
+                    Cell dataCell = new Cell().Add(new Paragraph(propiedad.GetValue(x).ToString())
+                        .SetFontSize(8) // tamaño de fuente
+                        .SetPadding(5)); // el relleno
+                    dataCell.SetTextAlignment(TextAlignment.CENTER);
+
+                    table.AddCell(dataCell);
+                }
+            }
+
+            // Agregar la tabla al documento
+            doc.Add(table);
+            doc.Close();
+
+            byte[] bytesStream = ms.ToArray();
+            ms = new MemoryStream();
+            ms.Write(bytesStream, 0, bytesStream.Length);
+            ms.Position = 0;
+
+            return new FileStreamResult(ms, "application/pdf");
+        }
     }
 }
